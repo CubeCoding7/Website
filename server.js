8761a9228f88e04496f2833b01c4cdd9e8bf31de
Cube Coding <sljohnston714@gmail.com>
2023-10-20 22:44:02 -0400

    added the routes for the pages I added

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const app = express()
 
+app.use((req, res, next) => {
+    res.locals.active = req.path;
+    next();
+});
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get("/", (req, res) => {
-    res.render("index")
-})
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
 
 app.listen(3000)
